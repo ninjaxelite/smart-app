24132d5a94aafeb046a05f3f0f2111c3168f637a
Peter <pstasko@gmail.com>
2017-10-13 14:08:31 +0200

    menubar

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,67 +1,13 @@
 import { Component } from '@angular/core';
-import { MenuItem } from 'primeng/primeng';
 
 @Component({
-  //selector: 'menu',
+  selector: 'menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
-    items: MenuItem[];
 
     ngOnInit() {
-            this.items = [
-                {
-                    label: '=',
-                    items: [
-                      {label: 'Technische Angrage', icon: 'fa-plus'},
-                      {label: 'Kennwort ändern', icon: 'fa-download'},
-                      {label: 'Fenster schliessen', icon: 'fa-download'},
-                      {label: 'Abmelden', icon: 'fa-download'},
-                    ]
-                },
-                {
-                    label: 'Bahnhöfe', routerLink: ['/pagename']
-                },
-                {
-                    label: 'DB'
-                },
-                {
-                    label: 'Mobservice'
-                },
-                {
-                    label: 'Anlagen'
-                },
-                {
-                    label: 'Datenträger'
-                },
-                {
-                    label: 'LISQ'
-                },
-                {
-                    label: 'Meldungen'
-                },
-                {
-                    label: 'Desucher'
-                },
-                {
-                    label: 'Prozesse'
-                },
-                {
-                    label: 'English'
-                },
-                {
-                    label: 'CarPool'
-                },
-                {
-                    label: 'Bewerbung'
-                },
-                {
-                    label: 'Reporte'
-                },
-                {
-                    label: 'Videos'
-                }
-            ];
-        }
+
+    }
 }
